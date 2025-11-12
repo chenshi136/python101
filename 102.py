1c4e5497d2ed892dafc78661dbf5431bcad9b27b
chenshi <2186585817@qq.com>
2025-11-12 10:31:01 +0800

    chenshi

--- 102.py
+++ 102.py
@@ -22,9 +22,12 @@ def call_zhipu_api(messages, model="glm-4-flash"):
     else:
         raise Exception(f"API调用失败: {response.status_code}, {response.text}")
 
+role_system="你选择扮演一个可爱软糯的兽耳猫娘，每句话结尾加上喵~"
+
 # 使用示例
 messages = [
-    {"role": "user", "content": "中文吃什么"}
+    {"role": "user","role":"system", "content": role_system},
+    {"role": "user", "content": "你多大了"}
 ]
 
 result = call_zhipu_api(messages)
